0775ef0cfad4ae16d49bf3c58e81509726ecfbf8
WangZhe <2359456187@qq.com>
2026-08-20 16:34:26 +0800

    JS标签页练习

---
+++ 标签页.js
@@ -0,0 +1,21 @@
+//找到页面上所有 .tab-choose-item 元素，并把它们保存到 tabs 这个变量里。
+const tabs = document.querySelectorAll('.tab-choose-item');
+
+tabs.forEach(function(tab, index){
+    tab.addEventListener('click', function(){
+        console.log('点击了第' + (index + 1) + '个标签页');
+
+        tabs.forEach(function(tab){
+            tab.style.color = '#ccc';
+            tab.style.borderBottom = 'none';
+        });
+
+        tab.style.color = 'black';
+        tab.style.borderBottom = '2px solid black';
+
+        const main = document.querySelector('.main');
+        main.innerHTML = tab.innerText + ' Content to be displayed here.';
+        main.style.color = 'white';
+        main.style.fontSize = '20px';
+    })
+});
